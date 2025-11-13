bf9eb071133f86154c0ee7138e029d8e6097593a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-13 08:30:16 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/allgoal/AllGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.allgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllGoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
